ec02260d13ef093905024716c51640c8ef34badc
Maarten Depauw <mhkdepauw@gmail.com>
2021-12-02 11:43:51 +0100

    puzzle2 complete

---
+++ Opdrachten/opdracht2/puzzle2_part1.py
@@ -0,0 +1,19 @@
+def file_to_list(filename):
+    puzzle = open(filename, "r")
+    list1 = puzzle.readlines()
+    return list1
+
+def main():
+    horizontal_position = 0
+    depth = 0
+    list_of_file = file_to_list("input_puzzle2")
+    for i in range((len(list_of_file))):
+        if "down" in list_of_file[i]:
+            depth += int(list_of_file[i][-2])
+        elif "forward" in list_of_file[i]:
+            horizontal_position += int(list_of_file[i][-2])
+        else:
+            depth -= int(list_of_file[i][-2])
+    return horizontal_position*depth
+
+print(main())
